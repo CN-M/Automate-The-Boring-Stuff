13f0eab9ee750e6f3dd61dfca23e16754340e07c
Ntsako Mbhalati <cn.mbhalati@gmail.com>
2022-01-15 15:10:02 +0200

    continue course and uploading files

---
+++ 2.1.py
@@ -0,0 +1,9 @@
+x = 2
+y = 9
+Age = 23
+pet = 'dog'
+
+if Age ==23 and pet == 'dog':
+    print('True!')
+else:
+    print('False!')
---
+++ 2.2.py
@@ -0,0 +1,20 @@
+#name = 'Jann'
+
+print('What is your name?')
+name = input('Enter a name: ')
+
+if name != '':
+    print('Thank you for entering a name')
+else:
+    print('Enter a name, bitch')
+
+print("The type of input is: " + str(type(name)))
+
+#if name == 'John':
+ #   print('Hi ' + name + '! Your work is ready!')
+#elif name == 'Jan':
+ #   print('Hey, ' + name + ". You shouldn't be using the computer right now!")
+#else:
+ #   print('Sup, ' + name + " I don't know you!")
+print('Done!')
+

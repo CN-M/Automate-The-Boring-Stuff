1b908c4f05f459dde61a030b2828b61c2a7c6d10
Ntsako Mbhalati <cn.mbhalati@gmail.com>
2022-01-18 16:29:02 +0200

    Continue course and uploading files

---
+++ 6.2.py
@@ -0,0 +1,23 @@
+x = range(1, 1001, 5)
+y = list(x)
+
+#print(y)
+
+supplies = ['pens','staplers','flame-throwers','kites']
+
+stat, cat, rat, chat = supplies
+
+stat, cat, rat, chat = 1, 2, 3, 4
+a = 'AAA'
+b = 'BBB'
+
+a, b = b, a
+
+print(a)
+print(b)
+
+for i in range(len(supplies())):
+    print('Index ' + str(i) + ' in supplies is: ' + supplies[i]) 
+
+print(rat)
+

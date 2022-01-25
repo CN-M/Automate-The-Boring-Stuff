fb838e9a2e0de7f8cd28f0dce19e63b432ca14b3
Ntsako Mbhalati <cn.mbhalati@gmail.com>
2022-01-25 20:19:42 +0200

    Continue course and uploading files

---
+++ Marks.py
@@ -0,0 +1,13 @@
+Subj = 3900 * 10
+Admin = 1880
+Src = 590
+Books_Acco = 8740.50 + (3542.50*2)
+
+
+Debit = Subj + Admin + Src + Books_Acco
+Credit = 1000 + 3500 + 500 + 10000 + (1500*2)
+
+Balance = Debit - Credit
+
+print('R'+ str(Balance))
+
